8811279e27f78da5cd56b034842a3ee96f4535f2
angmcruz <108002776+angmcruz@users.noreply.github.com>
2024-07-17 14:47:45 -0500

    STOP2

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+
+//TEMA
+
+// porque no pueden estar los 2 ?
+// el servidor no sabe cual elegir ya que tienen la 
+//misma ruta
+//no hacen lo mismo. send envia el mensaje
+//render : carga el crud que se transforma hasta html
+
+
+
+ /* GET users listing. */
+ router.get('/', function(req, res, next) {
+    
+  /* 1. Renderización de la vista crud.ejs */
+  res.render('crud');
+   
 });
+
+
 
 module.exports = router;
