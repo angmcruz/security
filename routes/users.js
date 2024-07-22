c5b92cbbbe8727ef738b21bb5977abca89572520
angmcruz <108002776+angmcruz@users.noreply.github.com>
2024-07-22 14:13:10 -0500

    upd

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+
+ /* 1. Importe el módulo crypto */
+ let crypto = require('crypto');
 
  /* 1. Cargue los modelos de acuerdo con la configuración de la conexión */
  const sequelize = require('../models/index.js').sequelize;
@@ -12,13 +11,18 @@ var router = express.Router();
  var models = initModels( sequelize );  
 
 
-//TEMA EX
+//TEMA EXAMEN 
 
 // porque no pueden estar los 2 ?
 // el servidor no sabe cual elegir ya que tienen la 
 //misma ruta
 //no hacen lo mismo. send envia el mensaje
 //render : carga el crud que se transforma hasta html
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
 
  /* GET users listing. */
@@ -33,7 +37,39 @@ var router = express.Router();
 
 });
 
-module.exports = router;
+ /* POST user. */
+ /* 2. Cree el callback asíncrono que responda al método POST */
+ router.post('/', async (req, res) => {
+
+  /* 3. Desestructure los elementos en el cuerpo del requerimiento */
+  let { name, password, idrole } = req.body;
+
+  try {
+
+    /* 4. Utilice la variable SALT para encriptar la variable password. */
+    let salt = process.env.SALT
+    let hash = crypto.createHmac('sha512', salt).update(password).digest("base64");
+    let passwordHash = salt + "$" + hash
+
+    /* 5. Guarde el registro mediante el método create */
+    let user = await models.users.create({ name: name, password: passwordHash })
+
+    /* 6. Redireccione a la ruta con la vista principal '/users' */
+    res.redirect('/users')
+
+  } catch (error) {
+
+    res.status(400).send(error)
+
+  }
+
+})
+
+
+
+
 
 
 module.exports = router;
+
+
