4325dca8b91b0091d9309351ee9079ae9b67fb85
angmcruz <108002776+angmcruz@users.noreply.github.com>
2024-07-29 14:13:12 -0500

    funka :)

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+ /* 1. Módulo express-session */
+const session = require('express-session');
+var authenticateSession = require('./middleware/authentication_session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -17,13 +19,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+
+/* config del middleware */
+app.use(session({
+  secret: process.env.TOKEN_SECRET,
+  name: 'session.security', 
+  resave: false,
+  saveUninitialized: false,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
+ /* 2. Agregue el middleware al router */
+ app.use('/users', authenticateSession, usersRouter);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
